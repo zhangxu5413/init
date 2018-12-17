e8e4b2a8a14f80270f4282080d223cfd84b9d67d
zxNice <z541315621@163.com>
2018-12-17 19:40:27 +0800

    Add files via upload

---
+++ src/main/java/com/zx/App.java
@@ -0,0 +1,17 @@
+package com.zx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description
+ *
+ * @author zx
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
